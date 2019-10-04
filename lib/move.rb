4289b7b1838bf48bdba94e5b51599e1d617dbd99
Albert Martinez <albertwdd@gmail.com>
2019-10-04 09:25:28 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -11,21 +11,17 @@ def input_to_index(input)
 end
 
 def move(arr, indx, char="X")
-  # char = char == "X" ? "X" : "O"
 	# puts "CHECKPOINT 1\narr: #{arr},\nindx: #{indx}\nchar: #{char}"
 	if arr[indx] == " "
-	 # arr[indx.to_i, 1]
-		# arr.insert(indx.to_i, char)
-		
-		# puts arr # check if arr was updated
-	else
- 		puts "Oops! Space #{indx.to_s} is taken. Try again"
-    input = gets.strip
-    indx = input_to_index(input.to_i)
-	end
+	  arr[indx, 1] = char
+	  else
+	    puts "Oops! Space #{indx.to_s} is taken. Try again"
+      input = gets.strip
+      indx = input_to_index(input.to_i)
+  end
 
 	if indx == 0 or indx == 1
-    	arr[indx.to_i] = char
+    	arr[indx.to_i===] = char
 	end
 	if indx.to_i >= 8
     	arr[8] = char
